fd092b05383dc7b27a350556b41d45f826430177
Christian Gazmuri <christiangazmo@gmail.com>
2021-08-16 02:51:37 -0400

    Got aggregation working with 200 line files

--- ETL/aggregation.js
+++ ETL/aggregation.js
@@ -1,29 +1,74 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ProductReviews',
-{ useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true});
-const Schema = mongoose.Schema
+var MongoClient = require('mongodb').MongoClient;
 
-console.log('hello')
-var reviews = mongoose.model("reviews", new Schema({}), "Reviews");
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/", function (err, db) {
+     if(err) throw err;
 
-var pipeline = [
-  { $addFields: { photos: []}},
-  {
-    "$lookup": {
-        "from": "Photos",
-        "localField": "id",
-        "foreignField": "review_id",
-        "as": "photos"
-    }
-  },
-  { $sort : { id : 1} },
-  { $limit: 5 }
-]
+     var dbo = db.db("ProductReviews");
 
-reviews.aggregate(pipeline, function (err, result) {
-  if (err) {
-      console.log(err);
-      return;
-  }
-  console.log(result[4].photos);
+     var finalProductPipe = [
+       { $group: { _id: '$product_id' }},
+       { $sort: { _id: 1} },
+       {
+          $lookup : {
+          "from": "newProduct",
+          "localField": "_id",
+          "foreignField": "product_id",
+          "as": "characteristics"
+          }
+       },
+       {
+        $lookup : {
+          "from": "ReviewsWithPhotos",
+          "localField": "_id",
+          "foreignField": "product_id",
+          "as": "results"
+          }
+       },
+       { $project: { 'characteristics.product_id': 0, 'results.product_id': 0, 'results.photos.review_id': 0} },
+       { $out: 'finalProducts' }
+     ]
+
+     var charPipe = [
+       { $project: { characteristic_id : 1, value : 1 }},
+       { $group: { _id : "$characteristic_id", value : { $avg: '$value'} }},
+       { $sort : { _id : 1} },
+       { $out: 'newCharReviews' }
+     ]
+
+     var productPipe = [
+      {
+        $lookup : {
+          "from": "newCharReviews",
+          "localField": "id",
+          "foreignField": "_id",
+          "as": "info"
+      }
+      },
+      {
+        $replaceRoot: { newRoot: { $mergeObjects: [ { $arrayElemAt: [ "$info", 0 ] }, "$$ROOT" ] } }
+      },
+      { $project: { info: 0} },
+      { $out: "newProduct" }
+    ]
+     //Write databse Insert/Update/Query code here..
+     var pipeline = [
+      { $addFields : { photos: [] } },
+      {
+        $lookup : {
+            "from": "Photos",
+            "localField": "id",
+            "foreignField": "review_id",
+            "as": "photos"
+        }
+      },
+      { $sort : { id : 1} },
+      { $out: 'ReviewsWithPhotos'}
+    ];
+
+    dbo.collection("ReviewsWithPhotos").drop();
+    dbo.collection("ReviewChar").aggregate(charPipe).toArray();
+    dbo.collection("Reviews").aggregate(pipeline).toArray();
+    dbo.collection("Characteristics").aggregate(productPipe).toArray();
+    dbo.collection("Characteristics").aggregate(finalProductPipe).toArray();
 });
